631decec0c78b09030b70ac17db683dca7e4ae62
James Mitchell <jmitchell238@gmail.com>
2022-11-24 21:48:21 -0600

    Finish Lesson 19 and Add Lombok (#6)

--- src/main/java/components/Sprite.java
+++ src/main/java/components/Sprite.java
@@ -1,36 +1,54 @@
 package components;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.joml.Vector2f;
 import renderer.Texture;
 
-import java.util.Vector;
-
 public class Sprite {
 
-    private Texture texture;
-    private Vector2f[] texCoords;
+    @Getter
+    @Setter
+    private Texture texture = null;
 
-    public Sprite(Texture texture) {
-        this.texture = texture;
-        Vector2f[] texCoords = {
+    @Getter
+    @Setter
+    private Vector2f[] texCoords = {
                 new Vector2f(1, 1),
                 new Vector2f(1, 0),
                 new Vector2f(0, 0),
                 new Vector2f(0, 1)
-        };
-        this.texCoords = texCoords;
-    }
+    };
 
-    public Sprite(Texture texture, Vector2f[] texCoords) {
-        this.texture = texture;
-        this.texCoords = texCoords;
-    }
+//    public Sprite(Texture texture) {
+//        this.texture = texture;
+//        Vector2f[] texCoords = {
+//                new Vector2f(1, 1),
+//                new Vector2f(1, 0),
+//                new Vector2f(0, 0),
+//                new Vector2f(0, 1)
+//        };
+//        this.texCoords = texCoords;
+//    }
+//
+//    public Sprite(Texture texture, Vector2f[] texCoords) {
+//        this.texture = texture;
+//        this.texCoords = texCoords;
+//    }
 
-    public Texture getTexture() {
-        return this.texture;
-    }
-
-    public Vector2f[] getTexCoords() {
-        return this.texCoords;
-    }
+//    public Texture getTexture() {
+//        return this.texture;
+//    }
+//
+//    public Vector2f[] getTexCoords() {
+//        return this.texCoords;
+//    }
+//
+//    public void setTexture(Texture tex) {
+//        this.texture = tex;
+//    }
+//
+//    public void setTexCoords(Vector2f[] texCoords) {
+//        this.texCoords = texCoords;
+//    }
 }
