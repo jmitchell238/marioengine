636808dcb1a519db0415806be5922ed436ada3eb
James Mitchell <jmitchell238@gmail.com>
2022-11-26 13:10:41 -0600

    Finished Lesson 20

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -148,6 +148,8 @@ public class Window {
         float endTime;
         float dt = -1.0f;
 
+        currentScene.load();
+
         while (!glfwWindowShouldClose(glfwWindow)) {
             // Poll events
             glfwPollEvents();
@@ -176,6 +178,8 @@ public class Window {
             dt = endTime - beginTime;
             beginTime = endTime;
         }
+
+        currentScene.saveExit();
     }
 
     public static int getWidth() {
