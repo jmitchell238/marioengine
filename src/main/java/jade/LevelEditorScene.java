654ceade36b1af263e10f5b9a57e35577c14eb24
James Mitchell <jmitchell238@gmail.com>
2022-11-26 13:12:00 -0600

    Finished Lesson 20 (#7)

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -25,8 +25,10 @@ public class LevelEditorScene extends Scene{
     @Override
     public void init() {
         loadResources();
-
         this.camera = new Camera(new Vector2f(-250, 0));
+        if(levelLoaded) {
+            return;
+        }
 
         sprites = AssetPool.getSpriteSheet("assets/images/spritesheet.png");
 
@@ -46,14 +48,6 @@ public class LevelEditorScene extends Scene{
         obj2SpriteRenderer.setSprite(obj2Sprite);
         obj2.addComponent(obj2SpriteRenderer);
         this.addGameObjectToScene(obj2);
-
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
-        String serialized = gson.toJson(obj1);
-        System.out.println(serialized);
-        GameObject obj = gson.fromJson(serialized, GameObject.class);
-        System.out.println(obj);
     }
 
     private void loadResources() {
